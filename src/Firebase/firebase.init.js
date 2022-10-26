681e83fde4b4d8a6f708ada7ba5e3cd3ebf96608
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-10-27 00:34:22 +0600

    initialization firebase

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDfCV9s8DErBK45jab2rjxQH127EQjQHbc",
+  authDomain: "future-educators.firebaseapp.com",
+  projectId: "future-educators",
+  storageBucket: "future-educators.appspot.com",
+  messagingSenderId: "734208150437",
+  appId: "1:734208150437:web:2a0324499d732d501cf64c",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
